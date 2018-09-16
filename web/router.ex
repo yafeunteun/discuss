e03a72601c9b88f6ea05aefe13c98b461f1fe786
yafeunteun <yannfeunteun@gmail.com>
2018-09-16 18:09:46 +0200

    refactor: typo in the route to create a topic

--- web/router.ex
+++ web/router.ex
@@ -19,7 +19,7 @@ defmodule Discuss.Router do
     get "/", PageController, :index
 
     get "/topics/new", TopicController, :new
-    post "/topics/create", TopicController, :create
+    post "/topics", TopicController, :create
 
   end
 
